0be953568cddc30bc57dd8f5babdbcf8baa47a3f
DarLaIl <darlail.work.mail@gmail.com>
2024-11-20 15:31:06 +0300

    32. bug fix

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -10,7 +10,6 @@ import { Avatar } from '../../components/dashboard-page/Avatar/Avatar';
 import { ProfileHeader } from '../../components/dashboard-page/ProfileHeader/ProfileHeader';
 import { TaskLists } from '../../components/dashboard-page/TasksLists/TasksLists';
 import { SharedTasks } from '../../components/dashboard-page/SharedTasks/SharedTasks';
-import { HolidaysToday } from '../../components/dashboard-page/HolidaysToday/HolidaysToday';
 import { Modal } from '../../components/Modal/Modal/Modal';
 import { Notification } from '../../components/dashboard-page/Notification/Notification';
 import styles from './Dashboard.module.css';
@@ -43,7 +42,6 @@ const Dashboard = async () => {
                     <div className={styles.userTasks}>
                         <TaskLists lists={taskLists} cookieValue={token} />
                         <SharedTasks tasks={assignedTasks} />
-                        {/*<HolidaysToday holidays={holidaysToday} />*/}
                     </div>
                     <Notification cookieValue={token} />
                     <Modal cookieValue={token} />
